2583be0b4bde2c0054d3ff0b57876b731ade3258
lexmin0412 <zhangle_dev@outlook.com>
2023-12-08 23:13:55 +0800

    chore: 添加路由及静态资源前缀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	base: '/astro-space',
+	site: 'https://lexmin0412.github.io/astro-space',
+	build: {
+		assetsPrefix: '/astro-space/'
+	}
+});
